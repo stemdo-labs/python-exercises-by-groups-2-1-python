4eae10152b2cd63fe5af8a7cd4b3a8201908b64b
icasado01 <icasado@stemdo.io>
2024-04-17 17:39:43 +0200

    Añadidas soluciones del módulo 2

--- soluciones/icasado-modulo2/icasado-conversiones/conversiones.py
+++
@@ -1,8 +0,0 @@
-# Declaramos la variable
-num1 = "1234"
-
-# Convertimos la varible en int
-num1 = int(num1)
-
-# Imprimir el tipo de la variable
-print(type(num1))
--- soluciones/icasado-modulo2/icasado-tipos de datos/num_data.py
+++
@@ -1,6 +0,0 @@
-num1 = 7.5
-num2 = 2.5
-
-suma = num1 + num2
-print(type(suma))
-
