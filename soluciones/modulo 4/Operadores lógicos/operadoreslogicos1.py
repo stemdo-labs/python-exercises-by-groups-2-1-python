af4c6cf7e3d66911913f43f4e04feefa62f0a2af
Pilar_Alonso <palonso@stemdo.io>
2024-04-17 13:33:06 +0200

    feat: add operadoreslogicos1

--- soluciones/modulo 4/Operadores lógicos/operadoreslogicos1.py
+++ soluciones/modulo 4/Operadores lógicos/operadoreslogicos1.py
@@ -1,9 +1,15 @@
-Crea tres variables **(num1 ,  num2 y num3)**:
+#Crea tres variables **(num1 ,  num2 y num3)**:
 
-- Dentro de **num1**, almacena el valor **36**
+#- Dentro de **num1**, almacena el valor **36**
 
-- Dentro de **num2**, almacena el resultado de la operación **72/2**
+# Dentro de **num2**, almacena el resultado de la operación **72/2**
 
-- Dentro de **num3**, almacena el valor **48**
+# Dentro de **num3**, almacena el valor **48**
 
-Verifica si **num1** es mayor que **num2**, y menor que **num3**. Almacena el resultado de dicha comparación en una variable llamada **mi_bool**.
+#Verifica si **num1** es mayor que **num2**, y menor que **num3**. Almacena el resultado de dicha comparación en una variable llamada **mi_bool**.
+
+num1=36
+num2=72/2
+num3=48
+mi_bool=num1>num2 and num1<num3
+print("El resultado es "+ str(mi_bool)+ "ya que no cumple los dos crterios")
