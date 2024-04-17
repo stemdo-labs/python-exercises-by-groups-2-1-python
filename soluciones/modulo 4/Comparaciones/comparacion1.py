7b67fa4754706312bc572b90fd5ba58e94b72795
Pilar_Alonso <palonso@stemdo.io>
2024-04-17 13:28:29 +0200

    feat: add exercise comparacion1

---
+++ soluciones/modulo 4/Comparaciones/comparacion1.py
@@ -0,0 +1,6 @@
+#Crea dos variables **(num1 y  num2)** con los valores 36 y 17 respectivamente. 
+#Verifica si **num1** es **mayor o igual** que num2 y almacena el resultado de dicha comparación en una variable llamada **mi_bool**
+num1=36
+num2=17
+mi_bool=num1>=num2
+print("Resultado de la  comparación es: "+str(mi_bool))
